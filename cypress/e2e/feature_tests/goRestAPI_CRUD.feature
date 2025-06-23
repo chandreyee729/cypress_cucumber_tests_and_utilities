47499271f3799698dee185df157c9a622207aa7c
chandreyee729 <chandreyeechakrabortyster@gmail.com>
2025-06-23 22:16:52 +0200

    new feature test for GoRest

--- cypress/e2e/feature_tests/goRestAPI_CRUD.feature
+++ cypress/e2e/feature_tests/goRestAPI_CRUD.feature
@@ -1,4 +1,5 @@
 Feature: Cucumber - User CRUD Operations with GoRest APIs
+
     Scenario Outline: Verify successful update of User name and status using GoRest network requests
         Given a "male" and "inactive" user "<username>" is created
         When the User Name is updated to "<updatedUsername>" using user id
@@ -11,3 +12,14 @@ Feature: Cucumber - User CRUD Operations with GoRest APIs
             | Alice    | Alice GoRest    | active            |
             | Newton   | GoRest Newton   | active            |
 
+    Scenario Outline: Verify user can create comments on a new post using GoRest network requests
+        Given a "male" and "inactive" user "<username>" is created
+        When a new post stating "<postStatement>" is created using user id
+        Then user can fetch the post with "<postStatement>" using user id
+        When a new comment "<commentStatement>" is added using post id
+        Then user can fetch comment with "<commentStatement>" from post using post id
+
+
+        Examples:
+            | username | postStatement  | commentStatement                 |
+            | Marcella | This is a post | This is a comment about Marcella |
--- cypress/support/step_definitions/goRestAPI_CRUD.steps.js
+++ cypress/support/step_definitions/goRestAPI_CRUD.steps.js
@@ -2,6 +2,7 @@ import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor';
 
 const token = Cypress.env('GOREST_PRIMARY_TOKEN');
 let email;
+const uuid = Date.now();
 
 Given("a {string} and {string} user {string} is created", (gender, status, name) => {
     email = `cypress.${name}${Date.now()}@mailinator.com`;
@@ -22,8 +23,9 @@ When("the User Name is updated to {string} using user id", (updatedUserName) => 
                 name: updatedUserName,
             }
         }).then(response => {
+            expect(response.status).to.eq(200);
             cy.wrap(response).as('updatedUser');
-            cy.log(`Now Username "${user.name}" updated to "${response.body.name}"`)
+            cy.log(`Now Username "${user.name}" updated to "${response.body.name}"`);
         })
     })
 });
@@ -41,8 +43,9 @@ When("the User Status is updated to {string} using user id", (newStatus) => {
                 status: newStatus,
             }
         }).then(response => {
+            expect(response.status).to.eq(200);
             cy.wrap(response).as('updatedUser');
-            cy.log(`Username "${user.status}" updated to "${response.body.status}"`)
+            cy.log(`Username "${user.status}" updated to "${response.body.status}"`);
 
         })
     })
@@ -62,5 +65,88 @@ Then("the User's {string} is changed successfully to {string}", (userDetails, up
             expect(updateResponse.body.status).to.eq(update, `User status is ${update}`);
         }
         else { cy.log("Only name or gender or status can be updated") }
+    })
+})
+
+When("a new post stating {string} is created using user id", (post) =>{
+    cy.get('@newUser').then(user => {
+        cy.log(`User id : ${user.id}`);
+        cy.request({
+            method: 'POST',
+            url: `/public/v2/users/${user.id}/posts`,
+            headers: {
+                authorization: `Bearer ${token}`
+            },
+            body: {
+                title: `${uuid}_${user.id}`,
+                body: post
+            }
+        }).then(response => {
+            expect(response.status).to.eq(201);
+            cy.wrap(response).as('newUserPost');
+            cy.log(`Now User "${user.name}" has a new post of id "${response.body.id}" stating "${response.body.body}"`);
+        })
+    })
+})
+
+Then("user can fetch the post with {string} using user id",(post) => {
+    cy.get('@newUserPost').then(userPost => {
+        cy.request({
+            method: 'GET',
+            url: `/public/v2/users/${userPost.body.user_id}/posts`,
+            headers: {
+                authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            expect(response.status).to.eq(200);
+            const hasPostId = response.body.some(comment => comment.post_id === userPost.post_id);
+            expect(hasPostId).to.be.true;
+            cy.wrap(response.body).as('newUserPosts');
+        })
+    })
+})
+
+When("a new comment {string} is added using post id", (comment) =>{
+    email = `user_${Math.floor(Math.random() * 100000)}@commentor.com`;
+    cy.get('@newUserPost').then(post => {
+        cy.log(`User id : ${post.body.user_id}`);
+        cy.log(`Post id : ${post.body.id}`);
+            cy.request({
+                method: 'POST',
+                url: `/public/v2/posts/${post.body.id}/comments`,
+                headers: {
+                    authorization: `Bearer ${token}`
+                },
+                body: {
+                    post: 'true'    ,
+                    name: 'Commenter Stroganoff',
+                    email: email,
+                    body: comment
+                }
+            }).then(response => {
+                expect(response.status).to.eq(201);
+                cy.wrap(response.body).as('newPostComment');
+                cy.log(`Now User Post of id "${response.body.post_id}" has a comment from "${response.body.name}" and "${response.body.email}"`);
+        })
+    })
+})
+
+Then("user can fetch comment with {string} from post using post id",(comment) => {
+    cy.get('@newUserPost').then(post => {
+        cy.log(`User id : ${post.body.user_id}`);
+        cy.log(`Post id : ${post.body.id}`); 
+        cy.request({
+            method: 'GET',
+            url: `/public/v2/posts/${post.body.id}/comments`,
+            headers: {
+                authorization: `Bearer ${token}`
+            }
+            }).then(response => {
+                expect(response.status).to.eq(200);
+                const hasCommentId = response.body.some(comment => comment.id === response.body[0].id);
+                //later fetch the comment id from response of '@newPostComment'
+                expect(hasCommentId).to.be.true;
+                expect(response.body[0].body).eq(comment);  
+        })
     })
 })
